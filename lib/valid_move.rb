02fe542d13fcdc3f9df9b14ebc0945b4c9880dec
McKayla Lankau <lankaukk@yahoo.com>
2020-07-19 22:10:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 def valid_move?(board, index)
   if position_taken?(board, index) == "true" ||
-    index.between?(0, 8) 
+    index > 8 ||
+    index < 0
     return false
   else 
     return true
